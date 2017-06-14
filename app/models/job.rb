fd4d82a991df92f1a024b99e8811e031e6c0f31b
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-13 23:00:53 -0500

    Added validations for presence and salary greater or equal to 0 and numerical

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :manager
+  validates :title, :description, :salary, presense: true
+  validates :salary, numericality: true
+  validates :salary, greater_than_or_equal_to: 0
 end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,3 +1,4 @@
 class Manager < ApplicationRecord
   has_many :job
+  validates :name, :title, presense: true
 end
