683a90c58e2086b945bda390a37fadee5b2f34cd
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-13 20:35:53 -0500

    Migrated jobs model
      $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614011353) do
+ActiveRecord::Schema.define(version: 20170614013200) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.decimal "salary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "managers", force: :cascade do |t|
     t.string "name"
