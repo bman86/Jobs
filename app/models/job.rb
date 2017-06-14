a3f6a3bf8e8ea72ee7006a696e728c8a44466925
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-14 15:06:15 -0500

    modified models for validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :manager
-  validates :title, :description, :salary, presense: true
-  validates :salary, numericality: true
-  validates :salary, greater_than_or_equal_to: 0
+  validates :title, :description, presence: true
+  validates :salary, numericality: {greater_than_or_equal_to: 0}
 end
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -4,4 +4,23 @@ class JobTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "must have a title description and salary" do
+    @job = Job.new
+    assert @job.invalid?
+    assert @job.errors[:title].any?
+    assert @job.errors[:description].any?
+    assert @job.errors[:salary].any?
+  end
+
+  test "salary must be greater than or equal to zero" do
+    job = Job.new(title: "Joe Blow",
+                  description: "Good Job")
+    job.salary = -1
+    assert job.invalid?
+    assert_equal ["Must be greater than or equal to 0"],
+      job.errors[:salary]
+
+    job.salary = 1
+    assert job.valid?
+  end
 end
